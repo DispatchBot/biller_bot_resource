6a57a99914593a29b474fc35dbfd556cd4b8563f
Brad Seefeld <bradley.seefeld@gmail.com>
2013-08-20 17:38:28 -0700

    API key should be thread sensitive

--- lib/biller_bot_resource/configuration.rb
+++ lib/biller_bot_resource/configuration.rb
@@ -1,8 +1,19 @@
 class BillerBotResource::Configuration
-  attr_accessor :site, :logger, :api_key, :timeout
-  
+  attr_accessor :site, :logger, :timeout
+
   def initialize
     self.site = "https://billerbot.com"
-    self.timeout = 10 # seconds. Low value so we fail fast
+    self.timeout = 7 # seconds. Low value so we fail fast
+  end
+
+  ##
+  # We store the API key within the context of the thread because this is more
+  # likely to change between requests.
+  def api_key=(key)
+    Thread.current["active.resource.currentthread.api_key"] = key
+  end
+
+  def api_key
+    Thread.current["active.resource.currentthread.api_key"]
   end
 end
