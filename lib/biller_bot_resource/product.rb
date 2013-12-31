da2689ffd5a5b9569f89f42b7554ab495eecb731
Brad Seefeld <bradley.seefeld@gmail.com>
2013-12-30 19:23:31 -0800

    Add some more convenience methods. Give the actual product to the context instead of just the ID

--- lib/biller_bot_resource/product.rb
+++ lib/biller_bot_resource/product.rb
@@ -3,10 +3,10 @@ class BillerBotResource::Product < BillerBotResource::Resource
   class Context < BillerBotResource::Resource
     self.prefix = "/products/:product_id/"
 
-    attr_accessor :product_id
+    attr_accessor :product
 
     def prefix_options
-      { :product_id => product_id }
+      { :product_id => product.id }
     end
 
     ##
@@ -18,7 +18,7 @@ class BillerBotResource::Product < BillerBotResource::Resource
 
       # Propogate to all children too.
       contexts.each do |c|
-        c.product_id = product_id
+        c.product = product
         c.force_persisted
       end
     end
@@ -29,7 +29,7 @@ class BillerBotResource::Product < BillerBotResource::Resource
     # @param  [String] type The type of context to be searching for
     # @return [Array<BillerBotResource::Product::Context] The matching contexts
     def children_with_type(type)
-      matches = contexts.select { |c| (c.type.downcase == type.to_s.downcase) }
+      contexts.select { |c| (c.type.downcase == type.to_s.downcase) }
     end
 
     ##
@@ -39,12 +39,40 @@ class BillerBotResource::Product < BillerBotResource::Resource
     def descendants_with_self
       [self, contexts.map(&:descendants_with_self)].flatten.uniq
     end
+
+    def parent
+      return nil unless product
+      product.contexts.each do |c|
+        r = c.with_child(self)
+        return r if r
+      end
+
+      nil
+    end
+
+    def with_child(context)
+      return self if contexts.include?(context)
+      contexts.each do |c|
+        result = c.with_child(context)
+        return result if result
+      end
+
+      nil
+    end
+
+    def parent_quantity
+      if parent && parent.quantity
+        parent.quantity
+      else
+        0
+      end
+    end
   end
 
   def initialize(*args)
     super
     contexts.each do |c|
-      c.product_id = id
+      c.product = self
       c.force_persisted # See comments on method
     end
   end
@@ -59,7 +87,7 @@ class BillerBotResource::Product < BillerBotResource::Resource
   end
 
   def context(id)
-    context = all_contexts.select { |c| c.id.try(:to_i) == id.try(:to_i) }.first
+    context = all_contexts.select { |c| c.id.try(:to_i) == id.to_i }.first
     raise ActiveResource::ResourceNotFound.new(nil) if context.nil?
     context
   end
